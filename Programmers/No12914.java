f1bf1906381f6365293e9afc07ebe10054698225
Mung <44817568+kimungung@users.noreply.github.com>
2023-02-19 22:25:54 +0900

    Programmers 12914 풀이

---
+++ Programmers/No12914.java
@@ -0,0 +1,27 @@
+/*
+Programmers 12914 멀리 뛰기
+
+
+*/
+
+class Solution {
+    public long solution(int n) {
+        long[] dp = new long[n+1];
+        
+        dp[1] = 1;
+        
+        if(n != 1){
+            dp[2] = 2;
+        }
+        
+        
+        for(int i=3; i<=n; i++){
+            
+            dp[i] = (dp[i-1]+dp[i-2])%1234567;
+            
+        }
+        
+        
+        return dp[n];
+    }
+}
